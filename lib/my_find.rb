f3fa9c609dc2d8a34ca5c8aaacb956325551519d
Jashema Panapa <jcpanapa@gmail.com>
2020-04-01 05:43:18 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,8 +4,7 @@ def my_find(collection)
   i = 0
   while i < collection.length
    return collection[i]
-  if collection = (1..100).to_a
-my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
+  if 
     yield(collection[i])
     i = i + 1
   end
